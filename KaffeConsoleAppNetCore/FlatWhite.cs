e7c501d6f42b42742eb23a3c15269a426815002f
lucalarsen <lucalarsen@hotmail.com>
2019-02-01 13:50:06 +0100

    den er ikke helt færdig

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    class FlatWhite : Kaffe, Imælk
+    {
+        public FlatWhite(int rabat) 
+            : base(rabat)
+        {
+            
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "mild";
+        }
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -9,7 +10,30 @@ namespace KaffeConsoleAppNetCore
         {
             Console.WriteLine("Hello World!");
 
-            
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            SortKaffe sortkaffe = new SortKaffe();
+            FlatWhite flatWhite = new FlatWhite(12);
+
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+            liste.Add(flatWhite);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("før cast" + item.MlMælk());
+                if (item is Latte)
+                {
+                    Latte i = item as Latte;
+                    Console.WriteLine("latte styrke" + i.styrke());
+                }
+                else if (item is Cortado) 
+                {
+                    Cortado c = item as Cortado;
+                }
+               
+            }
 
            
 
